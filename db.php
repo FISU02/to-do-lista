787b425312c100278c9a5cb3cce68ce82a8d526f
FISU02 <119970288+FISU02@users.noreply.github.com>
2024-06-24 22:38:55 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,11 +1,10 @@
 <?php
-$hostname=todo-lista-server.mysql.database.azure.com;
-$username=rzaztnvlxd;
-$password='Alamakota123';
-$ssl-mode=require;
+$servername="todo-lista-server.mysql.database.azure.com";
+$username="rzaztnvlxd@todo-lista-server";
+$password="Alamakota123";
 $dbname = "todo";
 
-$conn = new mysqli($hostname, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
